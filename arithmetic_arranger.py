3f61ff4947deecaa12decd1c25d76b69bd4b43d2
Bjørnar Borge <72221999+bjoernarborge@users.noreply.github.com>
2022-12-21 14:44:50 +0100

    Update arithmetic_arranger.py

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,36 +1,58 @@
-# Creating a function that receives a list of strings that are arithmetic problems
-# and returns the problems arranged vertically and side-by-side.
-# The function optionally takes a second argument. When the second argument is set to True, the answers are displayed.
 def arithmetic_arranger(problems, show_answers=False):
-    # Error: Too many problems. More than 5 problems.
+    # If length of problems is greater than 5, return error message
     if len(problems) > 5:
         return "Error: Too many problems."
-    # Error: Operator must be '+' or '-'. Operator is not '+' or '-'.
-    for problem in problems:
-        if problem.split()[1] not in ['+', '-']:
-            return "Error: Operator must be '+' or '-'."
-    # Error: Numbers must only contain digits. Number contains a letter.
-    for problem in problems:
-        if not problem.split()[0].isdigit() or not problem.split()[2].isdigit():
-            return "Error: Numbers must only contain digits."
-    # Error: Numbers cannot be more than four digits. Number is more than 4 digits.
-    for problem in problems:
-        if len(problem.split()[0]) > 4 or len(problem.split()[2]) > 4:
-            return "Error: Numbers cannot be more than four digits."
 
-    # If no errors, then arrange the problems vertically and side-by-side.
-    # There should be single space between the operator and the longest number.
-    # Numbers should be right aligned.
-    # There should be four spaces between each problem.
-    # There should be dashes at the bottom of each problem.
-    first_operand = []
-    second_operand = []
-    operator = []
-    for problem in problems:
-        first_operand.append(problem.split()[0])
-        second_operand.append(problem.split()[2])
-        operator.append(problem.split()[1])
-        
+    # List of all operations in str format
+    operations = list(map(lambda x: x.split()[1], problems))
+    if not all(map(lambda x: x in ["+", "-"], operations)):
+        return "Error: Operator must be '+' or '-'."
+
+    numbers = []  # list of all operands in str format
+    for i in problems:
+        p = i.split()
+        numbers.extend([p[0], p[2]])
+
+    if not all(map(lambda x: x.isdigit(), numbers)):
+        arranged_problems = "Error: Numbers must only contain digits."
+        return arranged_problems
+
+    if not all(map(lambda x: len(x) < 5, numbers)):
+        arranged_problems = "Error: Numbers cannot be more than four digits."
+        return arranged_problems
+
+    top_row = ''
+    dashes = ''
+    values = list(map(lambda x: eval(x), problems))
+    solutions = ''
+
+    for i in range(0, len(numbers), 2):
+        space_width = max(len(numbers[i]), len(numbers[i+1])) + 2
+        top_row += numbers[i].rjust(space_width)
+        dashes += '-' * space_width
+        solutions += str(values[i // 2]).rjust(space_width)
+        if i != len(numbers) - 2:
+            top_row += ' ' * 4
+            dashes += ' ' * 4
+            solutions += ' ' * 4
+
+    bottom_row = ''
+    for i in range(1, len(numbers), 2):
+        space_width = max(len(numbers[i - 1]), len(numbers[i])) + 1
+        bottom_row += operations[i // 2]
+        bottom_row += numbers[i].rjust(space_width)
+        if i != len(numbers) - 1:
+            bottom_row += ' ' * 4
+
+    if show_answers:
+        arranged_problems = '\n'.join((top_row, bottom_row, dashes, solutions))
+    else:
+        arranged_problems = '\n'.join((top_row, bottom_row, dashes))
+    return arranged_problems
+
+    
+
+    
    
 
 
